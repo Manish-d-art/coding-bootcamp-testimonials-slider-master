9d7b1584812f9a2bab1d8dfbace8647802292ca4
manish mandal <76776194mm@gmail.com>
2022-09-10 16:01:51 +0530

    update js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+'user strict'
+// console.log(2);
+
+const slides=document.querySelectorAll('.slide');
+const leftBtn=document.querySelector('.arrow-left');
+const rightBtn=document.querySelector('.arrow-right');
